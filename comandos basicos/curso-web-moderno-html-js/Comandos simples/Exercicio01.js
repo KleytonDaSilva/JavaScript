5fadb276fe2e17a6175e28d53a92d190b70d66fe
nksaddam <nksaddam@gmail.com>
2024-01-08 09:34:04 -0300

    ataualizacao 08/01/2024

---
+++ comandos basicos/curso-web-moderno-html-js/Comandos simples/Exercicio01.js
@@ -0,0 +1,8 @@
+let raio = 10;
+const PI = 3.1415;
+
+let totalArea =  PI * (raio * raio);
+let comprimento = 2 * PI * raio;
+
+console.log("Area: "  + totalArea);
+console.log("Comprimento:" + comprimento) 
